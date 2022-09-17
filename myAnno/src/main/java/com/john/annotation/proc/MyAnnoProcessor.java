68e26c6cd15a9613e109e9d25122295dc12a431a
John <besthezhaowen@163.com>
2022-09-17 20:45:00 +0800

    init

---
+++ myAnno/src/main/java/com/john/annotation/proc/MyAnnoProcessor.java
@@ -0,0 +1,21 @@
+package com.john.annotation.proc;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@SupportedAnnotationTypes("com.john.annotation.MyAnno")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class MyAnnoProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        System.out.println("#################$$$$$$$$$$$$$$$");
+        return false;
+    }
+
+}
---
+++ myService/src/main/java/com/john/service/HelloService.java
@@ -0,0 +1,9 @@
+package com.john.service;
+
+import com.john.annotation.MyAnno;
+
+@MyAnno("demo")
+public interface HelloService {
+
+
+}
